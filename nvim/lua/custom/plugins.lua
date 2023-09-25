c73bf89144e6a76c66486e56c47bb848f201a161
leonardtan13 <leonardtan13@gmail.com>
2023-09-25 08:22:31 +0800

    add goplugin

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -58,8 +58,29 @@ local plugins = {
       }
     end,
   },
-
-
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+  },
 
   -- To make a plugin not be loaded
   -- {
