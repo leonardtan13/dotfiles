e7cbf5ec40d11fbce88ef888c6c4de02738c10ae
leonard.tan <leonard.tan@bytedance.com>
2023-10-06 16:14:19 +0800

    add prefix logger snippet

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -4,6 +4,9 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    -- bytedance specific keybind for prefix logger
+    ["<leader>pl"] = { "ipl := prefix_logger.NewAutoPrefix()", "prefix logger snippet" },
   },
 }
 
